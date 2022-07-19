fd0ec3e0656fab51be1e6ed3a0805b120c9c660d
teller-hamster <teller.hamster@gmail.com>
2022-07-19 21:00:10 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', '');
+      b = prompt('На сколько его оцените?', '');
+      c = prompt('Один из последних просмотренных фильмов?', '');
+      d = prompt('На сколько его оцените?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
