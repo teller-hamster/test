efe8d0b31cea5bcf9b805586d4827873f3b993c2
teller-hamster <teller.hamster@gmail.com>
2022-07-19 21:29:24 +0300

    First changes

--- js/script.js
+++ js/script.js
@@ -17,3 +17,15 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+const newArr = []
+
+    const movie1 = 'TERMINATOR 30000'
+    const movie2 = 'Titanik'
+    const movie3 = 'Отчаянные домохозяйки'
+
+    newArr.push(movie1, movie2, movie3)
+
+    newArr.map(film => {
+        console.log(film)
+    })
